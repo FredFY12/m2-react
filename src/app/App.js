b56d344385598d823e1ab02751581f89e2ad73ef
FredFY12 <fred-fy12@yandex.ru>
2023-03-20 10:17:51 +0200

    22.03

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import Users from "./components/users";
+import Users from "./layouts/users";
+import { Route, Switch } from "react-router-dom";
+import Main from "./layouts/main";
+import Login from "./layouts/login";
+import NavBar from "./components/navBar";
 
 function App () {
-  return <Users />;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/users/:userId?" component={Users} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
